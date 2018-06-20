c5f7065cf5826aaa0a27b314e054c1ac2d2c9716
Keylin_D <keylin.05.01@gmail.com>
2018-06-20 09:14:54 -0400

    menu desplegable

---
+++ src/data.js
@@ -0,0 +1,18 @@
+// 1) `computeUsersStats(users, progress, courses)`
+window.computeUsersStats = (users, progress, courses) => {
+  const inputText = document.querySelector('input');
+  
+  btn.addEventListener('click')
+};
+// 2) `sortUsers(users, orderBy, orderDirection)`
+window.sortUsers = (users, orderBy, orderDirection) => {
+
+};
+// 3) `filterUsers(users, search)`
+window.filterUsers = (users, search) => {
+
+};
+// 4) `processCohortData(options)`
+window.processCohortData = (options) => {
+
+};

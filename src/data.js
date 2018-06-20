fcea5d80f4050ce2903abec75be3bcee2c556bd6
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-06-20 13:48:29 -0300

    Resolviendo conflictos

--- src/data.js
+++ src/data.js
@@ -1,12 +1,5 @@
 // 1) `computeUsersStats(users, progress, courses)`
 window.computeUsersStats = (users, progress, courses) => {
-<<<<<<< HEAD
-
-=======
-  const inputText = document.querySelector('input');
-  
-  btn.addEventListener('click')
->>>>>>> upstream/master
 };
 // 2) `sortUsers(users, orderBy, orderDirection)`
 window.sortUsers = (users, orderBy, orderDirection) => {
@@ -19,10 +12,6 @@ window.filterUsers = (users, search) => {
 // 4) `processCohortData(options)`
 window.processCohortData = (options) => {
 
-<<<<<<< HEAD
 };
 
 
-=======
-};
->>>>>>> upstream/master

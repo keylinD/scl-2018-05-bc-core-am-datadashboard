dacc325c3100398f1cf5ee61269fd84832daa94c
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-06-20 11:02:07 -0300

    modificacion css y html

---
+++ src/data.js
@@ -0,0 +1,18 @@
+// 1) `computeUsersStats(users, progress, courses)`
+window.computeUsersStats = (users, progress, courses) => {
+
+};
+// 2) `sortUsers(users, orderBy, orderDirection)`
+window.sortUsers = (users, orderBy, orderDirection) => {
+
+};
+// 3) `filterUsers(users, search)`
+window.filterUsers = (users, search) => {
+
+};
+// 4) `processCohortData(options)`
+window.processCohortData = (options) => {
+
+};
+
+
